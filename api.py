1466ae9f2cff434c1c841ba7e70a1fe78bcc8066
Somuuuu <102124508+Somuuuu@users.noreply.github.com>
2025-07-09 16:44:05 +0530

    final

--- api.py
+++ api.py
@@ -5,6 +5,6 @@ def post(long):
     return response.json()
     
 def get(short):
-    response = requests.get(f"http://127.0.0.1:8000/{short}")
+    response = requests.get(f"https://url-shortner-5r7w-3qirap44b-somus-projects-3ca5f015.vercel.app/{short}")
     return response.json()
 
