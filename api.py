d8034cacc11d09708762bee58862bbf29769dbd7
Somuuuu <102124508+Somuuuu@users.noreply.github.com>
2025-07-09 12:39:21 +0000

    from git

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
 import requests
 
 def post(long):
-    response = requests.post(f"http://127.0.0.1:8000/post?long={long}") 
+    response = requests.post(f"https://url-shortner-5r7w-3qirap44b-somus-projects-3ca5f015.vercel.app/post?long={long}") 
     return response.json()
     
 def get(short):
